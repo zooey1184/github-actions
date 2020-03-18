e88fb992835ab8392ba36c35747e1308793a3fde
张应颖 <1184762165@qq.com>
2020-03-18 10:20:58 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      this is about github action demo
+      试试看
     </div>
   );
 }
